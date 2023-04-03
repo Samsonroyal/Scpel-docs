c4c0d486d3e7db8350652d41122b525e3dc57603
Were Samson Bruno <werefast2000@gmail.com>
2023-04-03 12:16:55 +0300

    tweaks

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Scpel Docs📜</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Scpel-lang',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/Scpel',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Samsonroyal/scpel-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Scpel Docs',
   },
 }
 
